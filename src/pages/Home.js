7d0fea0c74e6c86e6360d5896d9858a972c90421
Sarath Kumar Dunga <sarathkumardunga09@gmail.com>
2023-08-23 21:16:45 +0530

    docs: Intitial content of HomePage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import EmailIcon from "@mui/icons-material/Email";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home">
+      <div className="about">
+        <h2>
+          Hi, My Name is <span>S</span>arath
+        </h2>
+        <div className="prompt">
+          <p>A software developer with a passion for learning and creating.</p>
+          <LinkedInIcon />
+          <EmailIcon />
+          <GitHubIcon />
+        </div>
+      </div>
+      <div className="skills">
+        <h1> Skills</h1>
+        <ol className="list">
+          <li className="item">
+            <h2> Front-End</h2>
+            <span>
+              ReactJS, Redux, HTML, CSS, Javascript, BootStrap, MaterialUI,
+              Chakra-UI
+            </span>
+          </li>
+          <li className="item">
+            <h2>Back-End</h2>
+            <span>NodeJS, ExpressJS, MySQL, MongoDB(intermediate)</span>
+          </li>
+          <li className="item">
+            <h2>Languages</h2>
+            <span>C++, C, JavaScript, TypeScript(intermediate)</span>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
