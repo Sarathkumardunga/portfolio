22b7e5b7fa6ed37cb7e6ccc9d633ec3a416d380c
Sarath Kumar Dunga <sarathkumardunga09@gmail.com>
2023-08-23 17:23:02 +0530

    docs: setup the links and routes, Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Education from './pages/Education';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Portfolio
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/education" element={<Education />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="toggleButton">
+        <button></button>
+      </div>
+
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/education">Education</Link>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar
