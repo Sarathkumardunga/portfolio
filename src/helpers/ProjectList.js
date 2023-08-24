0bcbf4f3fb12c074bfba703d7d485ee108a86d6a
Sarath Kumar Dunga <sarathkumardunga09@gmail.com>
2023-08-24 14:25:42 +0530

    Projeccts section with test project

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,17 @@
+import Proj1 from "../assets/mainpic.svg"
+import Proj2 from "../assets/pic.jpg"
+
+export const ProjectList = [
+  {
+    name: "Project1",
+    image: Proj1,
+  },
+  {
+    name: "Real Chat",
+    image: Proj2,
+  },
+  {
+    name: "Project1",
+    image: Proj1,
+  },
+];
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import "../styles/Projects.css";
+import { ProjectList } from "../helpers/ProjectList";
+import ProjectItem from '../components/ProjectItem';
 
 const Projects = () => {
   return (
-    <div>
-      Projects
+    <div className="projects">
+      <h1>My Personal Projects</h1>
+      <div className="projectList">
+        {
+          ProjectList.map((project, id) => {
+            return <ProjectItem name={ project.name } image={ project.image } />;
+          })
+        }
+      </div>
     </div>
   );
 }
