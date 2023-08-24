d8a233c07182bdfd62f0002aedfb6ce9e5e009b3
Sarath Kumar Dunga <sarathkumardunga09@gmail.com>
2023-08-24 22:35:39 +0530

    Added Projects and populated the links

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,20 +1,43 @@
-import Proj1 from "../assets/mainpic.svg"
-import Proj2 from "../assets/pic.jpg"
+import Proj1 from "../assets/ShadowFitness.png"
+import Proj2 from "../assets/RealChat.png"
+import Proj3 from "../assets/SortingVisualizer.png"
+import Proj4 from "../assets/MathsGame.png"
+import Proj5 from "../assets/CashRegister.png";
 
 export const ProjectList = [
   {
-    name: "Project1",
+    name: "Shadow Fitness",
     image: Proj1,
-    skills: "Javascript",
+    skills: "ReactJs, Javascript(ES6+), HTML5, CSS3",
+    github: "https://github.com/Sarathkumardunga/SHADOW-FITNESS",
+    deploy: "https://shadowfitness-sarath09.netlify.app/",
   },
   {
     name: "Real Chat",
     image: Proj2,
-    skills: "Javascript",
+    skills: "ReactJs, Socket.IO, MongoDB, Express, Javascript(ES6+), HTML5, CSS3",
+    github: "https://github.com/Sarathkumardunga/Real-Chat",
+    deploy: "https://github.com/Sarathkumardunga/Real-Chat",
   },
   {
-    name: "Project1",
-    image: Proj1,
-    skills: "Javascript",
+    name: "Sorting Visualizer",
+    image: Proj3,
+    skills: "Javascript(ES6+), HTML5, CSS3",
+    github: "https://github.com/Sarathkumardunga/Sorting-Visualizer",
+    deploy: "https://sarathkumardunga.github.io/Sorting-Visualizer/",
+  },
+  {
+    name: "Maths is Fun",
+    image: Proj4,
+    skills: "Javascript, HTML5, CSS3",
+    github: "https://github.com/Sarathkumardunga/Maths-Game",
+    deploy: "https://maths-game-sarath-kumar-dunga.netlify.app/",
+  },
+  {
+    name: "Cash Register App",
+    image: Proj5,
+    skills: "HTML5, CSS3 Javascript",
+    github: "https://github.com/Sarathkumardunga/Cash-Register-App",
+    deploy: "https://cashregister-sarath09.netlify.app/",
   },
 ];
--- src/pages/ProjectDisplay.js
+++ src/pages/ProjectDisplay.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useParams } from "react-router-dom"
 import { ProjectList } from '../helpers/ProjectList';
 import GitHubIcon from "@mui/icons-material/GitHub";
+import StreamIcon from "@mui/icons-material/Stream";
 import "../styles/ProjectDisplay.css"
 
 const ProjectDisplay = () => {
@@ -16,7 +17,29 @@ const ProjectDisplay = () => {
         <p>
           <b>Skills: </b> {project.skills}
         </p>
-        <GitHubIcon />
+
+        <div className="home-buttons" id="source-code">
+          <a
+            href={project.github}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="btn">
+              <GitHubIcon />
+              Source code
+            </button>
+          </a>
+          <a
+            href={project.deploy}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="btn" id="live-demo">
+              <StreamIcon />
+              Try Live
+            </button>
+          </a>
+        </div>
       </div>
       <div id="im">
         <img src={project.image} alt="Project" />
