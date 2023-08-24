0d9862aded45f1532a5ca68c554a1e9e05957a48
Sarath Kumar Dunga <sarathkumardunga09@gmail.com>
2023-08-25 02:07:03 +0530

    Added Profile picture

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Link, useLocation } from "react-router-dom";
 import "../styles/Navbar.css";
 import ReorderIcon from "@mui/icons-material/Reorder";
+import profilePic from "../assets/profilepic.png";
 
 const Navbar = () => {
   const [expandNavbar, setExpandNavbar] = useState(false);
@@ -15,15 +16,24 @@ const Navbar = () => {
   return (
     <div className="navbar" id={expandNavbar ? "open" : "close"}>
       <div className="toggleButton">
-        <button onClick = {() => {setExpandNavbar((prev)=> !prev)}}>
-          <ReorderIcon/>
+        <button
+          onClick={() => {
+            setExpandNavbar((prev) => !prev);
+          }}
+        >
+          <ReorderIcon />
         </button>
       </div>
 
-      <div className="links">
-        <Link to="/">Home</Link>
-        <Link to="/projects">Projects</Link>
-        <Link to="/education">Education</Link>
+      <div className='nav-content'>
+        <div id="profilepic">
+          <img src={profilePic} alt="Admin Pic" />
+        </div>
+        <div className="links">
+          <Link to="/">Home</Link>
+          <Link to="/projects">Projects</Link>
+          <Link to="/education">Education</Link>
+        </div>
       </div>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,92 +9,94 @@ import "../styles/Home.css";
 
 const Home = () => {
   return (
-    <div className="home">
-      <div className="top">
-        <div className="about">
-          <h2>
-            Hi, My Name is <span className="oneLetter">S</span>arath
-          </h2>
-          <div className="prompt">
-            <p>
-              Aspiring developer, dedicated to crafting innovative solutions
-              through coding.
-            </p>
-            <a
-              href="https://www.linkedin.com/in/sarath-kumar-dunga-7083021a0"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <LinkedInIcon />
-            </a>
+    <div>
+      <div className="home">
+        <div className="top">
+          <div className="about">
+            <h2>
+              Hi, My Name is <span className="oneLetter">S</span>arath
+            </h2>
+            <div className="prompt">
+              <p>
+                Aspiring developer, dedicated to crafting innovative solutions
+                through coding.
+              </p>
+              <a
+                href="https://www.linkedin.com/in/sarath-kumar-dunga-7083021a0"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <LinkedInIcon />
+              </a>
 
-            <a
-              href="mailto:sarathkumardunga09@gmail.com?"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <EmailIcon />
-            </a>
+              <a
+                href="mailto:sarathkumardunga09@gmail.com?"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <EmailIcon />
+              </a>
 
-            <a
-              href="https://github.com/Sarathkumardunga"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <GitHubIcon />
-            </a>
+              <a
+                href="https://github.com/Sarathkumardunga"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <GitHubIcon />
+              </a>
+            </div>
+            {/*     Buttons   */}
+            <div className="home-buttons">
+              <a
+                href="https://drive.google.com/file/d/1BOD0ljImvewrOYUHGygh6PCWmAC3cRAy/view?usp=drive_link"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <button className="btn" id="resume">
+                  <SimCardDownloadIcon />
+                  Resume
+                </button>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/sarath-kumar-dunga-7083021a0"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <button className="btn" id="connect">
+                  <HandshakeIcon />
+                  Connect
+                </button>
+              </a>
+            </div>
           </div>
-          {/*     Buttons   */}
-          <div className="home-buttons">
-            <a
-              href="https://drive.google.com/file/d/1BOD0ljImvewrOYUHGygh6PCWmAC3cRAy/view?usp=drive_link"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <button className="btn" id="resume">
-                <SimCardDownloadIcon />
-                Resume
-              </button>
-            </a>
-            <a
-              href="https://www.linkedin.com/in/sarath-kumar-dunga-7083021a0"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <button className="btn" id="connect">
-                <HandshakeIcon />
-                Connect
-              </button>
-            </a>
+
+          <div className="mainpic">
+            <img src={mainPic} alt="Center stage" />
           </div>
         </div>
 
-        <div className="mainpic">
-          <img src={mainPic} alt="Center stage" />
+        <div className="skills">
+          <h1>
+            <span className="oneLetter">S</span>kills
+          </h1>
+          <ol className="list">
+            <li className="item">
+              <h2> Front-End</h2>
+              <span>
+                ReactJS, Redux, HTML, CSS, Javascript, BootStrap, MaterialUI,
+                Chakra-UI
+              </span>
+            </li>
+            <li className="item">
+              <h2>Back-End</h2>
+              <span>NodeJS, ExpressJS, MySQL, MongoDB(intermediate)</span>
+            </li>
+            <li className="item">
+              <h2>Languages</h2>
+              <span>C++, C, JavaScript, TypeScript(intermediate)</span>
+            </li>
+          </ol>
         </div>
-      </div>
-
-      <div className="skills">
-        <h1>
-          <span className="oneLetter">S</span>kills
-        </h1>
-        <ol className="list">
-          <li className="item">
-            <h2> Front-End</h2>
-            <span>
-              ReactJS, Redux, HTML, CSS, Javascript, BootStrap, MaterialUI,
-              Chakra-UI
-            </span>
-          </li>
-          <li className="item">
-            <h2>Back-End</h2>
-            <span>NodeJS, ExpressJS, MySQL, MongoDB(intermediate)</span>
-          </li>
-          <li className="item">
-            <h2>Languages</h2>
-            <span>C++, C, JavaScript, TypeScript(intermediate)</span>
-          </li>
-        </ol>
       </div>
     </div>
   );
